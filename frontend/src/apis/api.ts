6a599e37e81606a0bd668e3efe9a12b0f252bcd2
트립비토즈 김수영 <ksy9926@naver.com>
2022-02-17 03:43:16 +0900

    Feat: proxy 서버 구축

---
+++ frontend/src/apis/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const fetchBooksInfo = async (query: string) => {
+  // 받아오는 res.data의 type이 axios.get<> 안에 들어감.
+  const res = await axios.get<any>(`http://localhost:8080?query=${query}`, {
+    headers: {
+      'X-Naver-Client-Id': 'JmRJSLl_LqA1HOTqhyZ6',
+      'X-Naver-Client-Secret': 'R1SgcgwrFp',
+    },
+  });
+  console.log('res', res)
+  return res
+};
